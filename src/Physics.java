ecef4699dd855ba0858722f1a79e9916059f2a32
Erik Steringer <deveriks@live.com>
2014-12-01 19:10:22 -0800

    javadoc'd Physics.java

--- src/Physics.java
+++ src/Physics.java
@@ -1,13 +1,3 @@
-/**
- * Physics.java
- *
- * Handles all of the physics for an object:
- *    - logic and overarching interface
- *    - collision detection (see CollisionImage.java)
- *    - graphical representation (see GraphicalImage.java)
- *
- * //FIXME: after GraphicalImage is implemented, run ":%s/\/\/GIMG/g" and fix all relevant fixmes.
- */
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Shape;
@@ -15,12 +5,15 @@ import org.newdawn.slick.geom.Transform;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-
-//DEBUG imports
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.fills.GradientFill;
 
+/**
+ * Class used to contain the physical properties of a StellarObject (motion, 
+ * location, and collisions).
+ *
+ */
 public abstract class Physics{
 
 	//position
@@ -36,15 +29,10 @@ public abstract class Physics{
 	private CollisionImage cimg;
 	private GraphicalImage gimg;
 
-	//------------------
-	//--| 'structors |--
-	//------------------
-
 	/**
 	 * Constructor
 	 * 
 	 * @param centroid A point representing the object's centroid; used as a point of reference in construction, a center of mass in "Physics Logic", and likewise in other places.
-	 * @param centroid_rot (optional) Specifies an initial rotation for the centroid.
 	 * @param cimg A collision image, which detects and handles collisions with other Physics instances.
 	 * @param gimg A graphical image, responsible for drawing this Physics instance on the screen.
 	 */
@@ -55,6 +43,15 @@ public abstract class Physics{
 		){
 		this(centroid, 0f, cimg, gimg);
 	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param centroid A point representing the object's centroid; used as a point of reference in construction, a center of mass in "Physics Logic", and likewise in other places.
+	 * @param centroid_rot Specifies an initial rotation for the centroid.
+	 * @param cimg A collision image, which detects and handles collisions with other Physics instances.
+	 * @param gimg A graphical image, responsible for drawing this Physics instance on the screen.
+	 */
 	public Physics(
 		Vector2f centroid,
 		float centroid_rot,
@@ -70,14 +67,13 @@ public abstract class Physics{
 		drot = 0f;
 	}
 
-	/** Destroys this, removing it from any/all push-notification systems */
+	/** 
+	 * Destroys this, removing it from any/all push-notification systems 
+	 *
+	 */
 	public void destroy(){
 		cimg.destroy();
 	}
-
-	//---------------------
-	//--| Physics Logic |--
-	//---------------------
 	
 	/**
 	 * Rotates this by dtheta degrees. Rotation is done once, not per-frame.
@@ -116,7 +112,11 @@ public abstract class Physics{
 			);
 	}
 
-	/** Returns theta as an angle in the range [0,2π] */
+	/** 
+	 * Returns theta as an angle in the range [0,2π] 
+	 *
+	 * @return An angle in the range [0, 2π] equal to the input angle.
+	 */
 	private float restrictAngle(float theta){
 		while (theta > float2pi) {
 			theta -= float2pi;
@@ -127,12 +127,13 @@ public abstract class Physics{
 		return theta;
 	}
 	private static final float float2pi = (float)Math.PI * 2f;
-	
-	//---------------
-	//--| Updates |--
-	//---------------
 
-	/** Performs framewise updates; should be propegated from the base Slick2D game object */
+	/** 
+	 * Performs framewise updates; should be propegated from the base Slick2D game object 
+	 *
+	 * @param gc The GameContainer needed for updating
+	 * @param time_passed_ms The time, in milliseconds, since the last 
+	 */
 	public void update(GameContainer gc, int time_passed_ms){
 		//update poisition (centroid)
 		centroid_pos = centroid_pos.add(velocity);
@@ -151,22 +152,40 @@ public abstract class Physics{
 		velocity = velocity.scale(friction);
 	}
 
-	/** Performs graphical updates; should be propegated from the base Slick2D game object */
+	/** 
+	 * Performs graphical updates; should be propegated from the base Slick2D game object 
+	 *
+	 * @param gc The GameContainer object needed for rendering
+	 * @param g The Graphics object needed for rendering
+	 */
 	public void render(GameContainer gc, Graphics g) throws SlickException{
 		gimg.render(gc, g);
 	}
 
-	//----------------------
-	//--| Access Methods |--
-	//----------------------
-	
-	/** Returns a copy of the centroid's position vector */
-	public Vector2f getPosition() { return new Vector2f(centroid_pos); }
+	/** 
+	 * Returns a copy of the centroid's position vector 
+	 *
+	 * @return The position of this object as a Vector2f
+	 */
+	public Vector2f getPosition() { 
+		return new Vector2f(centroid_pos); 
+	}
 
-	/** Returns the centroid's current rotation */
-	public float getRotation() { return centroid_rot; }
+	/** 
+	 * Returns the centroid's current rotation 
+	 *
+	 * @return The rotation of this object as a float
+	 */
+	public float getRotation() { 
+		return centroid_rot; 
+	}
 	
-	/** Returns the angle of the velocity of the object */
+	/** 
+	 * Returns the angle of the velocity of the object. If there's no velocity, 
+	 * a random angle is returned.
+	 *
+	 * @return A float for the velocity's angle. 
+	 */
 	public float getVelAngle() {
 		if (velocity.y == 0f && velocity.x == 0f)
 			return restrictAngle((float) Math.random() * float2pi);
@@ -174,24 +193,58 @@ public abstract class Physics{
 		return restrictAngle(result);
 	}
 	
-	/** Returns the speed of the object */
+	/** 
+	 * Returns the speed of the object 
+	 *
+	 * @return The speed of the object as a float
+	 */
 	public float getSpeed() {
 		float result = (float) Math.sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
 		return result;
 	}
 
-	/** Returns a copy of the velocity vector for this */
+	/** 
+	 * Returns a copy of the velocity vector for this 
+	 *
+	 * @return A Vector2f representing the velocity of the object.
+	 */
 	public Vector2f getVelocity(){
 		return velocity.copy();
 	}
 
-	/** Returns cimg, as it was passed to the constructor */
-	public CollisionImage getCImg() { return cimg; }
+	/** 
+	 * Returns cimg, as it was passed to the constructor 
+	 *
+	 * @return The CollisionImage object belonging to this object
+	 */
+	public CollisionImage getCImg() { 
+		return cimg; 
+	}
 
-	/** Returns gimg, as it was passed to the constructor */
-	public GraphicalImage getGImg() { return gimg; }
+	/** 
+	 * Returns gimg, as it was passed to the constructor 
+	 *
+	 * @return The GraphicalImage object belonging to this object
+	 */
+	public GraphicalImage getGImg() { 
+		return gimg; 
+	}
 
-	public void setFriction(float f){ friction = f; }
-	public float getFriction(){return friction;}
-
+	/**
+	 * Sets the friction of this object
+	 *
+	 * @param f The desired friction, between [0, 1] of this object.
+	 */
+	public void setFriction(float f){ 
+		friction = f; 
+	}
+	
+	/**
+	 * Gets the friction of the object
+	 *
+	 * @return The friction of this object as a float
+	 */
+	public float getFriction(){ 
+		return friction;
+	}
 }
